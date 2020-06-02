76e7c36a7c0c60355d3d58a384d20259029b2f53
majguo <jiangma@microsoft.com>
2020-06-02 14:31:21 +0800

    use bash instead of sh

--- test/scripts/gen-parameters-aad.sh
+++ test/scripts/gen-parameters-aad.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 parametersPath=$1
 githubUserName=$2
--- test/scripts/verify-deployments.sh
+++ test/scripts/verify-deployments.sh
@@ -10,13 +10,13 @@ scriptsDir="$6"
 
 # generate parameters for testing differnt cases
 parametersList=()
-sh ${scriptsDir}/gen-parameters.sh ${scriptsDir}/parameters.json $githubUserName $testbranchName
+bash ${scriptsDir}/gen-parameters.sh ${scriptsDir}/parameters.json $githubUserName $testbranchName
 parametersList+=(${scriptsDir}/parameters.json)
-sh ${scriptsDir}/gen-parameters-db.sh ${scriptsDir}/parameters-db.json $githubUserName $testbranchName
+bash ${scriptsDir}/gen-parameters-db.sh ${scriptsDir}/parameters-db.json $githubUserName $testbranchName
 parametersList+=(${scriptsDir}/parameters-db.json)
-sh ${scriptsDir}/gen-parameters-aad.sh ${scriptsDir}/parameters-aad.json $githubUserName $testbranchName
+bash ${scriptsDir}/gen-parameters-aad.sh ${scriptsDir}/parameters-aad.json $githubUserName $testbranchName
 parametersList+=(${scriptsDir}/parameters-aad.json)
-sh ${scriptsDir}/gen-parameters-db-aad.sh ${scriptsDir}/parameters-db-aad.json $githubUserName $testbranchName
+bash ${scriptsDir}/gen-parameters-db-aad.sh ${scriptsDir}/parameters-db-aad.json $githubUserName $testbranchName
 parametersList+=(${scriptsDir}/parameters-db-aad.json)
 
 # create Azure resources for preflight testing
